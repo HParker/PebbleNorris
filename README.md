42122094bd8a97984ee2bface40d6ca381abe3b6
Adam Hess <adamhess1991@gmail.com>
2015-09-03 10:17:44 -0600

    Create Chuck Norris App
    - get jokes from api.icndb.com

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pebble Norris
+
+Remember all the important facts we know about Chuck Norris!
+All conveniently located on your wrist
---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * The Chuck Norris app!
+ *
+ * Remember all the important facts we know about chuck norris.
+ */
+
+var UI = require('ui');
+var ajax = require('ajax');
+var url = "http://api.icndb.com/jokes/random";
+
+var main = new UI.Card({
+  title: 'Chuck Norris',
+  body: 'Press select.',
+  scrollable: true
+});
+
+main.on('click', 'select', function(event) {
+  ajax(
+    {
+      url: url,
+      type:'json'
+    },
+    function(data) {
+      main.subtitle(data.value.joke);
+    },
+    function(error) {
+      console.log('Download failed: ' + error);
+    });
+});
+
+main.show();
